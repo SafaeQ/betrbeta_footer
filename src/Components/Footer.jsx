465fea7dbc34fcfe8d31151cdbd3ab4b777deff0
SafaeQ <safaedani009@gmail.com>
2023-08-17 22:11:47 +0100

    ✨ add tailwind

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
-      
+      <footer className="bg-black text-white py-4">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default Footer;
